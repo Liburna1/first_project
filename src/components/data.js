77f43eef8511a0cfae4a17d30e50aa88a19c8817
Liburna Berisha <115898216+Liburna1@users.noreply.github.com>
2024-08-06 17:30:40 +0200

    Update data.js

--- src/components/data.js
+++ src/components/data.js
@@ -1,37 +1,94 @@
-import { FiFlag, FiShield } from "react-icons/fi";
-import { BiWalletAlt, BiKey } from "react-icons/bi";
-import { FaPlane } from "react-icons/fa";
-import { RiCustomerServiceFill } from "react-icons/ri";
-
-export const Data = [
+import logo1 from '../images/logo1t.jpg'
+export const Data4 = [
     {
-        icon:<FiFlag/>,
-        title:'HostPassword Scanner',
-        description:'Receiver alerts for compromised websites andd vulnerable password so you can take action to stay'
+        logo:logo1,
+        name:'Google Chrome',
+        description:'Receiver alerts for compromised websites andd vulnerable password so you can take action to stay',
+        link:'Get HostPaaword from Chome',
+        to:'/'
     },
     {
-        icon:<BiWalletAlt/>,
-        title:'Digital Wallet',
-        description:'Receiver alerts for compromised websites andd vulnerable password so you can take action to stay'
+        logo:logo1,
+        name:'Google Chrome',
+        description:'Receiver alerts for compromised websites andd vulnerable password so you can take action to stay',
+        link:'Get HostPaaword from Chome',
+        to:'/'
     },
     {
-        icon:<BiKey/>,
-        title:'Advanced Encryption',
-        description:'Receiver alerts for compromised websites andd vulnerable password so you can take action to stay'
+        logo:logo1,
+        name:'Google Chrome',
+        description:'Receiver alerts for compromised websites andd vulnerable password so you can take action to stay',
+        link:'Get HostPaaword from Chome',
+        to:'/'
     },
     {
-        icon:<FaPlane/>,
-        title:'Travel Mode',
-        description:'Receiver alerts for compromised websites andd vulnerable password so you can take action to stay'
+        logo:logo1,
+        name:'Google Chrome',
+        description:'Receiver alerts for compromised websites andd vulnerable password so you can take action to stay',
+        link:'Get HostPaaword from Chome',
+        to:'/'
     },
     {
-        icon:<FiShield/>,
-        title:'Total Privacy',
-        description:'Receiver alerts for compromised websites andd vulnerable password so you can take action to stay'
+        logo:logo1,
+        name:'Google Chrome',
+        description:'Receiver alerts for compromised websites andd vulnerable password so you can take action to stay',
+        link:'Get HostPaaword from Chome',
+        to:'/'
     },
     {
-        icon:<RiCustomerServiceFill/>,
-        title:'Unrivaled support',
-        description:'Receiver alerts for compromised websites andd vulnerable password so you can take action to stay'
+        logo:logo1,
+        name:'Google Chrome',
+        description:'Receiver alerts for compromised websites andd vulnerable password so you can take action to stay',
+        link:'Get HostPaaword from Chome',
+        to:'/'
     }
 ]
+
+
+export const FooterData = [
+    {
+      category: 'Category1',
+      linka: [
+        { link: 'Link1', to: '/' },
+        { link: 'Link2', to: '/' },
+        { link: 'Link3', to: '/' },
+        { link: 'Link4', to: '/' },
+      ],
+    },
+    {
+      category: 'Category2',
+      linka: [
+        { link: 'Link5', to: '/' },
+        { link: 'Link6', to: '/' },
+        { link: 'Link7', to: '/' },
+        { link: 'Link8', to: '/' },
+      ],
+    },
+    {
+        category: 'Category2',
+        linka: [
+          { link: 'Link5', to: '/' },
+          { link: 'Link6', to: '/' },
+          { link: 'Link7', to: '/' },
+          { link: 'Link8', to: '/' },
+        ],
+      },
+      {
+        category: 'Category2',
+        linka: [
+          { link: 'Link5', to: '/' },
+          { link: 'Link6', to: '/' },
+          { link: 'Link7', to: '/' },
+          { link: 'Link8', to: '/' },
+        ],
+      },
+      {
+        category: 'Category2',
+        linka: [
+          { link: 'Link5', to: '/' },
+          { link: 'Link6', to: '/' },
+          { link: 'Link7', to: '/' },
+          { link: 'Link8', to: '/' },
+        ],
+      },
+  ];
