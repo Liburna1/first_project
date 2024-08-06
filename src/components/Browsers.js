68d9d5db2d16885c36a20f4561b3fe5336d0efa8
Liburna Berisha <115898216+Liburna1@users.noreply.github.com>
2024-08-06 17:31:37 +0200

    Add files via upload

---
+++ src/components/Browsers.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Data4} from '../components/data'
+import './borwsers.scss'
+import { Link } from 'react-router-dom'
+
+const Browsers = () => {
+  return (
+    <div className='browser-items'>
+        {Data4.map((props) =>{
+            return(
+                <div className='item'>
+                    <div className='inline'>
+                        <div className='logo' style={{content: `url(${props.logo})`}}></div>
+                        <h6>{props.name}</h6>
+                    </div>
+                    <p>{props.description}</p>
+                    <Link to={props.to}>{props.link}</Link>
+                </div>
+            )
+        })}
+    </div>
+  )
+}
+
+export default Browsers
---
+++ src/components/NavData.js
@@ -0,0 +1,36 @@
+import { FiFlag, FiShield } from "react-icons/fi";
+import { BiWalletAlt, BiKey } from "react-icons/bi";
+
+export const NavData = [
+    {
+        icon:<FiFlag/>,
+        title:'Title1',
+        description:' Lorem Ispum dolor sit amet...'
+    },
+    {
+        icon:<FiShield/>,
+        title:'Title2',
+        description:' Lorem Ispum dolor sit amet...'
+    },
+    {
+        icon:<BiWalletAlt/>,
+        title:'Title3',
+        description:' Lorem Ispum dolor sit amet...'
+    },
+    {
+        icon:<BiKey/>,
+        title:'Title4',
+        description:' Lorem Ispum dolor sit amet...'
+    },
+    {
+        icon:<BiWalletAlt/>,
+        title:'Title3',
+        description:' Lorem Ispum dolor sit amet...'
+    },
+    {
+        icon:<BiKey/>,
+        title:'Title4',
+        description:' Lorem Ispum dolor sit amet...'
+    }
+    
+]
